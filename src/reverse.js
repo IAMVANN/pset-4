204fe0854ac2a5649bf65162a9bd3da7ef15d652
Ivan Zheng <IZheng@ucvts.tec.nj.us>
2019-11-13 10:37:47 -0500

    I did question 2 and fixed sum.js

--- src/reverse.js
+++ src/reverse.js
@@ -2,11 +2,51 @@ const scanner = require("readline-sync");
 let valid = false;
 console.log();
 let posInt;
+let numHolder = [];
+let position = 0;
+let divisor = 1;
+let divisorArray = [];
+let positionForDiv = 0;
+let printThis = "\n";
+let maxposition;
 while(valid === false){
     posInt = Number(scanner.question("Positive integer: "));
-    if( posInt < 1 || posInt > || Number.isNaN(posInt) == true){
+    if(posInt < 1 || Number.isSafeInteger(posInt) == false || Number.isNaN(posInt) == true){
         valid = false;
     } else {
         valid = true;
     }
 }
+let length = String(posInt).length;
+let lengthClone = length;
+for(length; length > 0; length--){
+    divisorArray[positionForDiv] = divisor;
+    divisor = divisor * 10;
+    if (length > 1){
+        positionForDiv++;
+    }
+}
+let currentNumber = posInt;
+while(positionForDiv >= 0){
+    numHolder[position] = (currentNumber - (currentNumber % divisorArray[positionForDiv]))
+      / divisorArray[positionForDiv];
+    currentNumber = currentNumber % divisorArray[positionForDiv];
+    if(positionForDiv == 0){
+        maxposition = position;
+    } else {
+        position++;
+    }
+    positionForDiv--;
+
+}
+while(position >= 0){
+
+    if(position == 0 ){
+        printThis = printThis + numHolder[position] + "."
+    } else {
+        printThis = printThis + numHolder[position] + ", ";
+    }
+    lengthClone--;
+    position--;
+}
+console.log(printThis);
--- src/sum.js
+++ src/sum.js
@@ -3,8 +3,10 @@ let valid = false;
 console.log();
 let lowerBound;
 let upperBound;
+let ogLowerbound
 while(valid === false){
     lowerBound = Number(scanner.question("Lower bound: "));
+    ogLowerbound = lowerBound;
     upperBound = Number(scanner.question("Upper bound: "));
     if(lowerBound >= upperBound || Number.isSafeInteger(lowerBound) == false ||
       Number.isSafeInteger(upperBound) == false || Number.isNaN(lowerBound) == true ||
@@ -16,7 +18,7 @@ while(valid === false){
 }
 let sum = 0;
 while(lowerBound <= upperBound){
-    if(lowerBound % 2 === 0){
+    if(lowerBound % 2 === 0 && lowerBound != ogLowerbound){
         sum = sum + lowerBound;
     }
     lowerBound++;
